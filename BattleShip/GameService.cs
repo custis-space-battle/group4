e07442f07ad1f8e7f798daa4bf8b238ebcd7d35a
Alexander Belinskiy <dikosasha@gmail.com>
2017-05-20 22:06:20 +0300

    jgjjg

--- BattleShip/GameService.cs
+++ BattleShip/GameService.cs
@@ -2,6 +2,7 @@
 using RabbitMQ.Client.Events;
 using System;
 using System.Text;
+using System.Threading;
 
 namespace BattleShip
 {
@@ -13,11 +14,12 @@ namespace BattleShip
         LetsShoot battle;
         Cell curShoot;
 
-        public void OnStart()
+        private ManualResetEvent _endEvent = new ManualResetEvent(false);
+
+        public void OnStart(string serverName, string modeName)
         {
             // Установить соединение
-            Console.WriteLine("Выберете сервер");
-            string serverName = Console.ReadLine();
+
             var connFactory = new ConnectionFactory { Uri = $"amqp://group4:z3bPjU@91.241.45.69/{serverName}" };
             var connection = connFactory.CreateConnection();
             channel = connection.CreateModel();
@@ -34,21 +36,18 @@ namespace BattleShip
             channel.QueueBind(incQueue, incQueue, incQueue);
             channel.BasicConsume(incQueue, true, consumer);
 
-            GetMode();
+            channel.BasicPublish(outQueue, outQueue, null, Encoding.UTF8.GetBytes($"start: { modeName }"));
 
             battle = new  LetsShoot();
             //curShoot = new Cell(2,2);
 
             consumer.Received += ProcessIncomingMessage;
+            _endEvent.WaitOne();
+
+            connection.Dispose();
         }
 
-        void GetMode()
-        {
-            Console.WriteLine("Выберете мод");
-            string modeName = Console.ReadLine();
-            channel.BasicPublish(outQueue, outQueue, null, Encoding.UTF8.GetBytes($"start: { modeName }"));
-            Console.WriteLine($"Выбран мод { modeName }\n");
-        }
+
 
         void ProcessIncomingMessage(object sender, BasicDeliverEventArgs e)
         {
@@ -57,15 +56,25 @@ namespace BattleShip
             // Передать координаты своих кораблей
             if (Encoding.UTF8.GetString(e.Body) == "prepare!")
             {
+                string[] dots = new string[3];
+
+                dots[0] = "2,6; 2,8; 2,9; 3,1; 3,2; 3,3; 3,4; 4,6; 5,6; 6,6; 6,9; 6,1; 6,2; 6,3; 10,1; 9,3; 9,4; 9,6; 9,7; 9,9"; // самый первый
+                dots[1] = "2,1; 2,2; 2,4; 2,5; 2,7; 2,8; 2,9; 2,10; 9,1; 9,2; 9,3; 9,5; 9,6; 9,8; 9,9; 9,10; 3,4; 6,5; 8,8; 4,8";// две вертикальные со смешением 
+                dots[2] = "1,1; 1,2; 1,3; 1,4; 1,6; 1,7; 1,8; 1,9; 1,10; 10,2; 10,3; 10,5; 10,6; 10,7; 10,9; 10,10; 5,3; 7,4; 2,2; 5,9"; // две вертикальные по краям
+                //dots[2] = 
+              
+                Random rnd = new Random();
+
+                int count = rnd.Next(0, 3);
                 // самая первая
-                 string dots = "2,1;2,2;2,3;2,4;6,1;6,2;6,3;9,2;9,3;3,6;4,6;5,6;8,5;9,5;7,7;4,8;1,9;1,10;6,10;9,9";
+              //   string dots = "2,1;2,2;2,3;2,4;6,1;6,2;6,3;9,2;9,3;3,6;4,6;5,6;8,5;9,5;7,7;4,8;1,9;1,10;6,10;9,9";
 
                 // для компа
                // string dots = "2,1;2,2;2,4;2,5;2,7;2,8;2,9;2,10;9,1;9,2;9,3;9,5;9,6;9,8;9,9;9,10;3,4;6,5;8,8;4,8";
 
                 // для игроков
                // string dots = "1,1; 1,2; 1,4; 1,5; 1,7; 1,8; 1,9; 1,10; 10,1; 10,2; 10,3; 10,5; 10,6; 10,8; 10,9; 10,10; 6,4; 8,5; 3,3; 5,7";
-                channel.BasicPublish(outQueue, outQueue, null, Encoding.UTF8.GetBytes(dots));
+                channel.BasicPublish(outQueue, outQueue, null, Encoding.UTF8.GetBytes(dots[count]));
                 Console.WriteLine($"Координаты отосланы");
             }
 
@@ -101,6 +110,12 @@ namespace BattleShip
             if (Encoding.UTF8.GetString(e.Body).Contains("fire result: KILL"))
             {
                 battle.AnalizaAns("KILL", curShoot);
+            }
+
+            // Передать координаты своих кораблей
+            if (Encoding.UTF8.GetString(e.Body).Contains("winner"))
+            {
+                _endEvent.Set();
             }
         }
     }
