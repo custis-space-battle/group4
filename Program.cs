11271903c0534b98c550ec2d39e703009d7d03c9
Alexander Belinskiy <dikosasha@gmail.com>
2017-05-20 12:57:20 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+
+namespace BattleShip
+{
+    class Program
+    {
+        void Main(string[] args)
+        {
+            // Установить соединение
+            var connFactory = new ConnectionFactory { Uri = "amqp://group4:z3bPjU" };
+            var connection = connFactory.CreateConnection();
+            var channel = connection.CreateModel();
+
+            // Настройка отправки сообщения
+            var outQueue = "group4";
+            channel.QueueDeclare(outQueue, exclusive: false);
+
+            // Отправить сообщение
+            byte[] body = new byte[1] { 255 };
+            channel.BasicPublish(outQueue, outQueue, null, body);
+
+            // Слушатель входящих сообщений
+            var consumer = new EventingBasicConsumer(;
+            var incQueue = "to_group4";
+            channel.QueueDeclare(incQueue, exclusive: false);
+            channel.QueueBind(incQueue, incQueue, incQueue);
+            channel.BasicConsume(incQueue, true, consumer);
+
+            consumer.Received += ProcessIncomingMessage;
+        }
+
+        void ProcessIncomingMessage(object sender, RabbitMQ.Client.Events.BasicDeliverEventArgs e)
+        {
+            Console.Write("Бот подключился");
+        }
+    }
+}
